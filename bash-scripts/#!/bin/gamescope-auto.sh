18eb021ccc9a758104feefed7149c0f35620e163
•C <ckorink@gmail.com>
2026-05-29 18:25:32 -0400

    cleanup a mistake

--- bash-scripts/#!/bin/gamescope-auto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-  while true; do
-		gamescope -w 1920 -h 1080 -W 1920 -H 1080 -r 60 -f --force-grab-cursor --display-index=0
-		echo "gamescope exited, restarting in 1 second..." >&2
-		sleep 1
-	done
